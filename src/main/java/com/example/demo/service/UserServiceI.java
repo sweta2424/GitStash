4177eb20287642e82da14dcd3d5ef228d223f263
sweta2424 <swetakumari2400@gmail.com>
2022-11-16 17:47:06 +0530

    PBA-O2 Service layer added

---
+++ src/main/java/com/example/demo/service/UserServiceI.java
@@ -0,0 +1,6 @@
+package com.example.demo.service;
+
+public interface UserServiceI {
+
+	public void save();
+}
---
+++ src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package com.example.demo.service;
+
+public class UserServiceImpl implements UserServiceI{
+
+	@Override
+	public void save() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
